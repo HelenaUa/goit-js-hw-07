b5090ee329d6602e642df61c220ef915e3b8c69c
HelenaUa <delena.1408@gmail.com>
2022-12-18 14:46:10 +0200

    add hw07 task2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -11,3 +11,39 @@ import { galleryItems } from './gallery-items.js';
 // Пусть подпись будет снизу и появляется через 250 миллисекунд после открытия изображения.
 
 console.log(galleryItems);
+
+const galleryEl = document.querySelector(".gallery");
+console.log(galleryEl);
+
+const arrayDiv = createGalleryMarkup(galleryItems);
+galleryEl.insertAdjacentHTML("beforeend", arrayDiv);
+
+function createGalleryMarkup(array) {
+//     return array.map(({ preview, original, description }) => `<a class="gallery__item"
+//  href="${original}">
+//     <img
+//       class="gallery__image"
+//       src="${preview}"
+//       alt="${description}"
+//     />
+//   </a>`).join(" ");
+    return array.reduce((acc, {preview, original, description}) => acc + `<a class="gallery__item"
+  href="${original}">
+   <img
+       class="gallery__image"
+       src="${preview}"
+       alt="${description}"
+     />
+   </a>`, '')
+};
+
+let lightbox = new SimpleLightbox('.gallery a', { 
+    captionsData: "alt",
+  captionDelay: 250,
+ });
+
+
+
+ 
+// var lightbox = new SimpleLightbox('.gallery a', { /* options */ });
+// console.log(lightbox);
